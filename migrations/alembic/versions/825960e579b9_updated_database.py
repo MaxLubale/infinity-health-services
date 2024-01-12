92198822605589f3c8aa7c6e74300684cfaf8ca4
Maxwell <lubalecliff@gmail.com>
2024-01-12 11:18:48 +0300

    Updated Database

---
+++ migrations/alembic/versions/825960e579b9_updated_database.py
@@ -0,0 +1,24 @@
+"""Updated Database
+
+Revision ID: 825960e579b9
+Revises: 6c0a62ae1c30
+Create Date: 2024-01-12 11:16:45.125359
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '825960e579b9'
+down_revision = '6c0a62ae1c30'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
